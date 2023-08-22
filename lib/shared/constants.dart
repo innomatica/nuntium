48d06cc234b34c032a56456c52c8194901536ec4
innomatic <innomatic@innomatic.ca>
2023-08-22 12:23:39 -0400

    version:1.6.2+25

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,7 +1,7 @@
 // app info
 const appId = 'com.innomatic.nuntiumapp';
 const appName = 'Nuntium';
-const appVersion = '1.6.1+24';
+const appVersion = '1.6.2+25';
 const emailDeveloper = 'nuntium.ubique@gmail.com';
 const urlPlayStore = 'https://play.google.com/store/apps/details?id=$appId';
 const urlAppStore = null;
